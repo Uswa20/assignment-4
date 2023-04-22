35d28eb752828b683e1c33a65cef75a141bc7688
Sebastian Csar <sacsar@gmail.com>
2023-04-22 16:32:59 -0400

    Move the server over and convert to TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,20 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
   },
-}
+  plugins: ["react-refresh", "@typescript-eslint", "jsx-a11y"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+};
